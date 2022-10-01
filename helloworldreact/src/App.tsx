19881186f76d78cd81fff3d22bcf7d87c6411d4c
zanattafleek <zanattafleek@gmail.com>
2022-10-01 12:08:22 -0300

    Componentes

--- helloworldreact/src/App.tsx
+++ helloworldreact/src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import CadastroCliente from './componentes/CadastroCliente'
+
 function App() {
+
+  const empresa: string = "Fleek Cursos"
+  const site: string = "https://www.fleekcursos.com.br"
+
+  const rsCliente = {
+    nome: 'Zanatta',
+    endereco: 'Av. 21 de Abril 504'
+  }
+
   return (
     <div className="App">
-      <h1>Fleek Cursos</h1>
+      <h1>{empresa}</h1>
+      <p>{10}</p>
+      <p>{rsCliente.nome}</p>
+      <p>{10 + 10}</p>
+      <a href={site}>Site Fleek</a>
+      <CadastroCliente />
+      <CadastroCliente />
+      <CadastroCliente />
     </div>
   );
 }
---
+++ helloworldreact/src/componentes/CadastroCliente.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './CadastroCliente.css'
+
+export default function CadastroCliente() {
+    return (
+        <div className='componenteCadastro'>Cadastro de Cliente</div>
+    )
+}
+
