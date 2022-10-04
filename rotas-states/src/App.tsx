fa21715f9b4415122087d5da0254ef59c6fbbca5
zanattafleek <zanattafleek@gmail.com>
2022-10-04 07:20:50 -0300

    ProjetoInicialStates

---
+++ rotas-states/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import CadastroCliente from './componentes/CadastroCliente'
+
+function App() {
+
+  return (
+    <div className="App">
+      <CadastroCliente />
+    </div>
+  );
+}
+
+export default App;
---
+++ rotas-states/src/componentes/CadastroCliente.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './CadastroCliente.css'
+
+export default function CadastroCliente() {
+    return (
+        <div className='componenteCadastro'>
+            <h1>Cadastro de Cliente</h1>
+<div className="textbox">
+    <label>Login:</label>
+    <input type="text" />
+</div>
+        </div>
+    )
+}
+
