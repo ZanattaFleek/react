c71d4e0bf6f2959c80871b78d48961ec27558396
zanattafleek <zanattafleek@gmail.com>
2022-10-04 08:43:29 -0300

    Eventos

--- helloworldreact/src/App.tsx
+++ helloworldreact/src/App.tsx
@@ -7,22 +7,10 @@ function App() {
 
   const empresa: string = "Fleek Cursos"
 
-  const UF: Array<string> = ['SP', 'MG', 'SC']
-
   return (
     <div className="App">
       <h1>{empresa}</h1>
-
-      {/* 
-
-      {UF.map((uf, index) => <p key={index}>{uf}</p>)}
-      
-*/}
-      {UF.map((uf, index) => {
-
-        return <p key={index}>{uf}</p>
-
-      })}
+      <CadastroCliente />
 
     </div>
   );
--- helloworldreact/src/componentes/CadastroCliente.tsx
+++ helloworldreact/src/componentes/CadastroCliente.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 
 import './CadastroCliente.css'
 
 export default function CadastroCliente() {
+
+    let nomeCliente: string = ""
+
+    const txtNomeOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+
+        nomeCliente = event.target.value
+
+    }
+
+    const txtNomeClick = () => {
+        alert('Oi '.concat(nomeCliente))
+    }
+
     return (
-        <div className='componenteCadastro'>Cadastro de Cliente</div>
+        <div className='componenteCadastro'>
+            <h2>Cadastro de Cliente</h2>
+
+            <label>Nome</label>
+            <input type="text" id="txtNome" onChange={txtNomeOnChange} />
+            <input type="button" value="Exibir Nome" onClick={txtNomeClick} />
+
+        </div>
     )
 }
 
